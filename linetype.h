c1dd6de53b82928d2acbad320b1543a5379e5ff8
Daniel Voyevoda <danielvoyevoda@outlook.com>
2025-04-10 00:30:09 -0400

    Set up main files

---
+++ linetype.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class lineType {
+public:
+    lineType() {
+
+    }
+private:
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<fstream>
+#include<iostream>
+
+#include "linetype.h"
+using namespace std;
+
+int main () {
+    
+}
